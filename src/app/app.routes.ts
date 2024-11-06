ce95579c3b88b8215a5a1e46dfa186f7b0425027
hamdi <hamdi.benjomaa@esprit.tn>
2024-11-06 15:59:03 +0100

    TEMPLATE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule) },
+  { path: 'Admin', loadChildren: () => import('./back-office/back-office.module').then(m => m.BackOfficeModule) },
+
+
+];
